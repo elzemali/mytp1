0d9be42ecfa36a601b586d647910ba70c12322d8
elaminezemali <108669576+elaminezemali@users.noreply.github.com>
2022-10-12 11:52:03 +0100

    ma premiere version

---
+++ app/src/main/java/com/example/mytp1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mytp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //32 < 30
+        int i =5;
+        //
+        int i;
+        i=5;
+
+        // 20
+    }
+
+
+
+
+}
